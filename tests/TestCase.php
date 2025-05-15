191e25f5cdfefc4abd2afad665428d2201dc53fb
Muhammad Irfan <mrfansi@outlook.com>
2025-05-15 16:41:57 +0700

    feat: add real Cloudflare API testing infrastructure with environment configuration

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,10 +3,20 @@
 namespace Mrfansi\L1\Test;
 
 use Mockery;
+use Dotenv\Dotenv;
 use Orchestra\Testbench\TestCase as Orchestra;
+use Mrfansi\L1\CloudflareKVConnector;
+use Mrfansi\L1\CloudflareQueuesConnector;
 
 abstract class TestCase extends Orchestra
 {
+    /**
+     * Flag to determine if real Cloudflare connections should be used
+     * 
+     * @var bool
+     */
+    protected bool $useRealCloudflare = false;
+
     /**
      * {@inheritdoc}
      */
@@ -14,12 +24,34 @@ abstract class TestCase extends Orchestra
     {
         parent::setUp();
 
+        // Load .env.testing file if it exists
+        $this->loadEnvironmentVariables();
+
+        // Check if we should use real Cloudflare
+        $this->useRealCloudflare = (bool) ($_SERVER['USE_REAL_CLOUDFLARE'] ?? false);
+
         // Skip migrations for KV and Queues tests
         if (!$this->isKVOrQueuesTest()) {
             $this->loadLaravelMigrations(['--database' => 'd1']);
         }
 
         $this->withFactories(__DIR__.'/database/factories');
+    }
+
+    /**
+     * Load environment variables from .env.testing file
+     */
+    protected function loadEnvironmentVariables(): void
+    {
+        if (file_exists(__DIR__ . '/../.env.testing')) {
+            $dotenv = Dotenv::createImmutable(__DIR__ . '/..', '.env.testing');
+            $dotenv->load();
+            
+            // Copy from .env to $_SERVER for Laravel's environment detection
+            foreach ($_ENV as $key => $value) {
+                $_SERVER[$key] = $value;
+            }
+        }
     }
 
     /**
@@ -41,6 +73,76 @@ abstract class TestCase extends Orchestra
         Mockery::close();
 
         parent::tearDown();
+    }
+
+    /**
+     * Create a KV connector instance (real or mock based on configuration)
+     * 
+     * @return \Mrfansi\L1\CloudflareKVConnector|\Mockery\MockInterface
+     */
+    protected function createKVConnector()
+    {
+        if ($this->useRealCloudflare) {
+            return new CloudflareKVConnector(
+                $_SERVER['CLOUDFLARE_TOKEN'],
+                $_SERVER['CLOUDFLARE_ACCOUNT_ID']
+            );
+        }
+
+        // Return a mock by default
+        $mock = Mockery::mock();
+        $mock->shouldReceive('__toString')->andReturn('MockedKVConnector');
+        return $mock;
+    }
+
+    /**
+     * Create a Queues connector instance (real or mock based on configuration)
+     * 
+     * @return \Mrfansi\L1\CloudflareQueuesConnector|\Mockery\MockInterface
+     */
+    protected function createQueuesConnector()
+    {
+        if ($this->useRealCloudflare) {
+            return new CloudflareQueuesConnector(
+                $_SERVER['CLOUDFLARE_TOKEN'],
+                $_SERVER['CLOUDFLARE_ACCOUNT_ID']
+            );
+        }
+
+        // Return a mock by default
+        $mock = Mockery::mock();
+        $mock->shouldReceive('__toString')->andReturn('MockedQueuesConnector');
+        return $mock;
+    }
+
+    /**
+     * Get the KV namespace ID for testing
+     * 
+     * @return string
+     */
+    protected function getKVNamespaceId(): string
+    {
+        return $_SERVER['CLOUDFLARE_KV_NAMESPACE_ID'] ?? 'test-namespace-id';
+    }
+
+    /**
+     * Get the KV namespace name for testing
+     * 
+     * @return string
+     */
+    protected function getKVNamespaceName(): string
+    {
+        return $_SERVER['CLOUDFLARE_KV_NAMESPACE_NAME'] ?? 'test-namespace';
+    }
+
+    /**
+     * Get the queue name for testing
+     * 
+     * @return string
+     */
+    protected function getQueueName(): string
+    {
+        return $_SERVER['CLOUDFLARE_QUEUE_NAME'] ?? 'test-queue';
     }
 
     /**
